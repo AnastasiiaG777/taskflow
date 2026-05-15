5bcb3c5bb39f601f4d4eb1a8ee2de828e4d33f08
«Анастасия <qetuofj777@mail.ru>
2026-05-15 18:12:55 +0400

    Add Task model with status choices

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'Новая'),
+        ('in_progress', 'В процессе'),
+        ('done', 'Выполнено'),
+    ]
+
+    title = models.CharField('Заголовок',max_length=100)
+    description = models.TextField('Описание')
+    status = models.CharField(
+        'Статус',
+        max_length=20,
+        choices= STATUS_CHOICES,
+        default='new'
+    )
+    created_at = models.DateTimeField('Время создания',  auto_now_add=True)
