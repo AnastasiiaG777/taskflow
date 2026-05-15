2dead1dd17572e1fca50ca6836fe6d0a06546a4f
«Анастасия <qetuofj777@mail.ru>
2026-05-15 18:38:12 +0400

    Improve Task admin with filters, search and short description

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'description_short', 'status', 'created_at')
+    list_display_links = ('pk', 'title')
+    ordering = ('pk',)
+    list_per_page = 10
+
+    list_filter = ('status', 'created_at')
+    search_fields = ('title',)
+
+
+    def description_short(self, obj:Task):
+        if len(obj.description) < 32:
+            return obj.description
+        return obj.description[:32] + '...'
+
+    description_short.short_description = 'Описание'
+
