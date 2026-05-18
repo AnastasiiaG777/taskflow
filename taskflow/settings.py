84b45efd9b5af4960576999fab6ac39b6187d547
«Анастасия <qetuofj777@mail.ru>
2026-05-18 20:35:57 +0400

    Prepare project for deployment

--- taskflow/settings.py
+++ taskflow/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kw1s*s=urncq&kx@#7z-7^kuhix+g%sr4h*s7rcn3cnkls-ilt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,6 +104,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'tasks'
